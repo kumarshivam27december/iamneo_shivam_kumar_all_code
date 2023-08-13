3d125058a6cc600110c6cb6484e46f386b424715
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-08-13 23:08:31 +0530

    version 3.0

---
+++ dsa/lec1_dsa_notes_deletion_of_last_element.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;cin>>a;int arr[a];
+    for (int i = 0; i < a; i++)
+    {
+        cin>>arr[i];
+    }
+    a--;
+    for (int  i = 0; i < a; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    
+    return 0;
+}
